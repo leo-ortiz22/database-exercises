65d765771f5a5b7ce026cf92f52f1194862729f1
leo-ortiz22 <101903651+leo-ortiz22@users.noreply.github.com>
2022-06-29 16:53:33 -0500

    Sql basic statements trying out different statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
+SHOW DATABASES;
+
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums
@@ -13,5 +15,43 @@ CREATE TABLE albums
     PRIMARY KEY (id)
 
 );
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
 
+CREATE TABLE quotes (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50),
+                        author_last_name  VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);
+
+DESCRIBE quotes;
+
+SHOW TABLES;
+
+SELECT author_last_name, content FROM quotes;
+
+UPDATE quotes
+SET author_first_name = 'Samuel', author_last_name = 'Clemens'
+WHERE id = 4;
+
+DELETE FROM quotes WHERE id = 3;
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams',    'Time is an illusion. Lunchtime doubly so.'),
+       ('Mark',    'Twain',    'Clothes make the man. Naked people have little or no influence on society.'),
+       ('Kurt',    'Vonnegut', 'The universe is a big place, perhaps the biggest.');
+
+SELECT * FROM quotes;
+
+SELECT * FROM quotes WHERE author_last_name = 'Adams';
+SELECT * FROM quotes WHERE id = 5;
+SELECT 'I am output!' AS 'Info';
+
+DELETE FROM quotes WHERE id = 3;
+
+-- First:
+SELECT * FROM quotes WHERE id = 3;
+-- Convert to:
+DELETE FROM quotes WHERE id = 3;
 
